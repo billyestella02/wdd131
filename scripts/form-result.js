153e0da260d2db96072e056bb015a89c0f27c854
Billy Estella <billyestella02@gmail.com>
2024-07-18 01:45:32 +0800

    updated review page

--- scripts/form-result.js
+++ scripts/form-result.js
@@ -1,11 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
-    let reviewCounter = localStorage.getItem('numReview') || 0;
+    let reviewCounter = window.localStorage.getItem('numReview') || 0;
     reviewCounter++;
     localStorage.setItem('numReview', reviewCounter);
 });
 
 // Display the click counter in results page
-const numberOfReviews = localStorage.getItem('numReview');
+const numberOfReviews = window.localStorage.getItem('numReview');
 const reviewCounter = document.querySelector('#review-counter');
 
 reviewCounter.innerHTML = numberOfReviews;
